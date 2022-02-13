94171dc2ce3b32b29a8f80e20230405b25431ed4
speedypleath <gheorgheandrei13@gmail.com>
2022-02-13 12:19:57 +0000

    update to python 3.10

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
         'License :: OSI Approved :: MIT License',
-        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
-    python_requires=">=3.6"
+    python_requires="3.10"
 )
